95b2f918a2de544a9bbb40f8ab5ad21c5687f2eb
niarenaw <niarenaw@gmail.com>
2025-06-16 17:18:39 -0700

    check in

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use ordinal::Ordinal;
+use num_traits::Pow;
+use ordinal::{CnfTerm, Ordinal};
 
 fn main() {
-    let _zero = Ordinal::zero();
-    let one = Ordinal::one();
-    let omega = Ordinal::omega();
+    let base = Ordinal::new_transfinite(&vec![
+        CnfTerm::new(&Ordinal::new_finite(3), 1).unwrap(),
+        CnfTerm::new(&Ordinal::new_finite(1), 1).unwrap(),
+    ])
+    .unwrap();
+    let expr = base.pow(Ordinal::new_finite(5));
+    let expected = Ordinal::new_transfinite(&vec![
+        CnfTerm::new(&Ordinal::new_finite(15), 1).unwrap(),
+        CnfTerm::new(&Ordinal::new_finite(13), 1).unwrap(),
+    ])
+    .unwrap();
 
-    let om1 = (one.clone() + one.clone()) * (omega + one.clone()) * (one.clone() + one.clone());
-    println!("{}", om1);
+    println!("Got:      {}", expr);
+    println!("Expected: {}", expected);
 }
